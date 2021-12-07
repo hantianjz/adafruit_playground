dba91e861625eb1fb26902523c8b300062d14f43
James Zhao <jamzhao@gmail.com>
2021-12-06 21:43:25 -0500

    Use CMSIS 5

--- src/main.c
+++ src/main.c
@@ -4,15 +4,26 @@
 
 #include "pio/samd21g18a.h"
 
+#include <stdbool.h>
+
+int sysTickHook(void) { return 0; }
+
 uint32_t SystemCoreClock;
 
-void SysTick_Handler(void) {
-  while (1)
-    ;
+void SysTick_DefaultHandler(void) {
+  while (1) {
+  };
 }
 
 int main(void) {
   SystemInit();
 
-  REG_PORT_OUT0 = PORT_PA17;
+  while (true) {
+    REG_PORT_OUT0 = PORT_PA17;
+
+    for (int i = 0; i < 100000; i++) {
+    }
+
+    REG_PORT_OUTCLR0 = PORT_PA17;
+  }
 }
