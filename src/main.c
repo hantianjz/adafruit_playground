808615fbeaced94f09ce714afd70193cb8422614
James Zhao <jamzhao@gmail.com>
2021-12-06 19:51:10 -0500

    Import external cmsis and arduino sdk repo

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
 #include <stdlib.h>
 
-int main(void) {}
+#include "samd21g18a.h"
+
+#include "instance/port.h"
+
+#include "pio/samd21g18a.h"
+
+uint32_t SystemCoreClock;
+
+void SysTick_Handler(void) {
+  while (1)
+    ;
+}
+
+int main(void) {
+  SystemInit();
+
+  REG_PORT_OUT0 = PORT_PA17;
+}
