cf995ffcdc318815db616eb534a9dd34a6c552cb
James Zhao <jamzhao@gmail.com>
2021-12-16 10:24:37 -0500

    Working blinky FW

--- src/main.c
+++ src/main.c
@@ -4,26 +4,19 @@
 
 #include "pio/samd21g18a.h"
 
-#include <stdbool.h>
+#include "api/ArduinoAPI.h"
+
+#include "variant.h"
 
 int sysTickHook(void) { return 0; }
 
-uint32_t SystemCoreClock;
+void yield(void) {}
 
-void SysTick_DefaultHandler(void) {
-  while (1) {
-  };
-}
+void setup(void) { pinMode(LED_BUILTIN, OUTPUT); }
 
-int main(void) {
-  SystemInit();
-
-  while (true) {
-    REG_PORT_OUT0 = PORT_PA17;
-
-    for (int i = 0; i < 100000; i++) {
-    }
-
-    REG_PORT_OUTCLR0 = PORT_PA17;
-  }
+void loop(void) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
